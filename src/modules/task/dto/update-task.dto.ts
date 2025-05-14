8b1bc0c6b995ae7bec15cac7e3bcd242fef13c25
sandy <sandydwi75@gmail.com>
2025-05-14 21:09:15 +0700

    feat: finish update task

---
+++ src/modules/task/dto/update-task.dto.ts
@@ -0,0 +1,27 @@
+import { IsBoolean, IsDateString, IsOptional, IsString } from "class-validator";
+
+export class UpdateTaskDto {
+  @IsOptional()
+  @IsString()
+  title: string;
+
+  @IsOptional()
+  @IsString()
+  description: string;
+
+  @IsOptional()
+  @IsDateString()
+  start_date: string;
+
+  @IsOptional()
+  @IsDateString()
+  end_date: string;
+
+  @IsOptional()
+  @IsBoolean()
+  is_done: boolean;
+
+  @IsOptional()
+  @IsString()
+  label_color: string;
+}
